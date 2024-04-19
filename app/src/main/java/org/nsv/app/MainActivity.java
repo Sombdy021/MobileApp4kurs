d6c5d5d93ae83945d93a74a57d122c832278e38a
Sombdy021 <git@github.com:Sombdy021>
2024-04-19 05:53:17 +0300

    Commit

---
+++ app/src/main/java/org/nsv/app/MainActivity.java
@@ -0,0 +1,50 @@
+package org.nsv.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button guessTheAnimal = findViewById(R.id.buttonGuessTheAnimal);
+        guessTheAnimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GuessTheAnimal.class));
+            }
+        });
+
+        Button wordPractice = findViewById(R.id.buttonWordPractice);
+        wordPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WordPractice.class));
+            }
+        });
+
+        Button listeningButton = findViewById(R.id.buttonListening);
+        listeningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Audition.class));
+            }
+        });
+
+        ImageView avatarButton = findViewById(R.id.imageAvatar);
+        avatarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Profile.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/org/nsv/app/Signup_2.java
@@ -0,0 +1,25 @@
+package org.nsv.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Signup_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup2);
+
+        TextView loginText = findViewById(R.id.textViewLogin);
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup_2.this, Login.class));
+            }
+        });
+    }
+}
